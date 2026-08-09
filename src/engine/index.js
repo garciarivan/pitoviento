1b76b5f528067c0b12bc646d4bd6f5d1fbdd0076
garciarivan <75338911+garciarivan@users.noreply.github.com>
2026-08-09 10:07:01 +0200

    Expone la nueva API modular del motor

---
+++ src/engine/index.js
@@ -0,0 +1,4 @@
+export { DeckParticleRenderer } from './DeckParticleRenderer.js';
+export { FlowModel, classFor, colorFor } from './flowModel.js';
+export { WindParticleEngine } from './WindParticleEngine.js';
+export * from './geo.js';
