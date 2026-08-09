8026e876a5b55fe7626acc3758c67012402ae7b7
garciarivan <75338911+garciarivan@users.noreply.github.com>
2026-08-09 10:47:08 +0200

    Expone la capa de partículas vectoriales GPU

--- src/engine/index.js
+++ src/engine/index.js
@@ -2,5 +2,6 @@ export { DeckParticleRenderer } from './DeckParticleRenderer.js';
 export { FlowModel, classFor, colorFor } from './flowModel.js';
 export { WindParticleEngine } from './WindParticleEngine.js';
 export { FlowWorkerClient } from './FlowWorkerClient.js';
+export { GpuVectorParticleLayer } from './GpuVectorParticleLayer.js';
 export { boundsAroundSite, buildTerrainGrid, createGridSampler } from './TerrainGrid.js';
 export * from './geo.js';
