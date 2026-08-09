4af97c475958954ffcf240e8b18e52b9f00fbc88
garciarivan <75338911+garciarivan@users.noreply.github.com>
2026-08-09 10:22:32 +0200

    Expone rejilla DEM y cliente del worker

--- src/engine/index.js
+++ src/engine/index.js
@@ -1,4 +1,6 @@
 export { DeckParticleRenderer } from './DeckParticleRenderer.js';
 export { FlowModel, classFor, colorFor } from './flowModel.js';
 export { WindParticleEngine } from './WindParticleEngine.js';
+export { FlowWorkerClient } from './FlowWorkerClient.js';
+export { boundsAroundSite, buildTerrainGrid, createGridSampler } from './TerrainGrid.js';
 export * from './geo.js';
